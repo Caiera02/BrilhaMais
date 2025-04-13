2046e7e040ccbba3f485b8df69ea0e6e20182a12
Caiera02 <caiocezar2807@gmail.com>
2025-04-12 22:15:30 -0300

    Inciando projeto

---
+++ vendedoras/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Representantes(models.Model):
+    nome = models.CharField(max_length=60)
+    rg = models.CharField(max_length=20)
+    cpf= models.CharField(max_length=20)
+    telefone = models.CharField(max_length=20)
+    telefone_2 = models.CharField(max_length=20, blank= True, null=True)
+    instagram= models.CharField(max_length=30)
+    ativo=models.BooleanField(default=True,verbose_name= 'ATIVO')
+    inativo=models.BooleanField(verbose_name= 'INATIVO')
+    data_criacao= models.DateField(auto_now_add=True,verbose_name= 'Data do cadastro')
+
