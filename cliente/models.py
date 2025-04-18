fcc2eb16bbc6289aa9c30fb2657f0007b6210855
Caiera02 <caiocezar2807@gmail.com>
2025-04-17 21:24:57 -0300

    arrumando def

--- cliente/models.py
+++ cliente/models.py
@@ -6,6 +6,9 @@ class Cliente (models.Model):
     nome = models.CharField(max_length=50)
     telefone = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.nome
+
 class Vendas (models.Model):
     nome =models.ForeignKey(Cliente,on_delete=models.CASCADE, related_name='clinte', verbose_name='Cliente')
     produto = models.ForeignKey(Produtos,on_delete=models.CASCADE, related_name='clinte', verbose_name='Peças')
@@ -14,4 +17,7 @@ class Vendas (models.Model):
     n_pago = models.BooleanField()
     data_compra = models.DateField(auto_now_add=True,verbose_name='Compra')
     data_atualizacao = models.DateField(auto_now=True,verbose_name='Atulização')
+
+    def __str__(self):
+        return self.nome
  
