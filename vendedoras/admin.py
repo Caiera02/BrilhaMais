e5186c4a474251ecff69da885f1a9ddaa875336d
Caiera02 <caiocezar2807@gmail.com>
2025-04-21 21:51:32 -0300

    Atualização do projeto, ajuste no models.maleta e outras coisas

--- vendedoras/admin.py
+++ vendedoras/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from vendedoras.models import Representantes,Produtos,Produto,Maleta
-
-@admin.register(Representantes)
-class RepresentantesAdmin(admin.ModelAdmin):
-    list_display= ('nome','cpf','telefone','rua','cep','bairro','numero','cidade','ativo','inativo',)
+from django.contrib.admin import DateFieldListFilter
+from datetime import date
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources, fields
+from import_export.widgets import ForeignKeyWidget
+from vendedoras.models import Representantes,Produto,Maleta,Produtos
 
 class ProdutoInline(admin.TabularInline):
     model = Produto
@@ -12,11 +13,49 @@ class ProdutoInline(admin.TabularInline):
 @admin.register(Maleta)
 class MaletaAdmin(admin.ModelAdmin):
     inlines = [ProdutoInline]
-    list_display = ['nome','consultora','data_criacao', 'valor_total']
+    list_display = ['consultora','nome','data_criacao','valor_total_formatado','comissaoVendedora','atualizacao']
+    search_fields = ['representantes__cpf']
+    
+    # def get_queryset(self, request):
+    #     qs = super().get_queryset(request)
 
-@admin.register(Produtos)
-class ProdutosAdmin(admin.ModelAdmin):
-    list_display = ['nome']
+    #     # Se o usuário não estiver filtrando manualmente por data,
+    #     # mostra apenas os registros de hoje
+    #     if 'data_criacao__gte' not in request.GET and 'data_criacao__exact' not in request.GET:
+    #         return qs.filter(data_criacao=date.today())
+    #     return qs
+
+@admin.register(Representantes)
+class RepresentantesAdmin(admin.ModelAdmin):
+    list_display= ('nome','cpf','telefone','rua','cep','bairro','numero','cidade','ativo','inativo',)
+
+class ProdutoResource(resources.ModelResource):
+    maleta = fields.Field(
+        column_name='maleta',
+        attribute='maleta',
+        widget=ForeignKeyWidget(Maleta, 'nome')  # usar nome do lote
+    )
+
+    tipos = fields.Field(
+        column_name='tipos',
+        attribute='tipos',
+        widget=ForeignKeyWidget(Produtos, 'nome')  # Exibe nome da peça
+    )
+
+    class Meta:
+        model = Produto
+        fields = ('maleta', 'tipos', 'valor')  # apenas os campos da planilha
+        import_id_fields = []  # <-- ESSENCIAL para evitar erro com 'id'
+        skip_unchanged = True
+        report_skipped = True
+
+@admin.register(Produto)
+class ProdutosAdmin(ImportExportModelAdmin):
+    resource_class = ProdutoResource
+    list_display = ['maleta','tipos', 'valor']
+    list_filter = [
+        ('maleta__data_criacao', DateFieldListFilter),
+    ]
     
 
     
--- vendedoras/models.py
+++ vendedoras/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from decimal import Decimal
+from django.utils.safestring import mark_safe
 
 class Representantes(models.Model):
     nome = models.CharField(max_length=60)
@@ -26,10 +28,35 @@ class Maleta(models.Model):
     nome = models.CharField(max_length=50, unique=True, verbose_name='Mostruario')
     consultora = models.ForeignKey(
         Representantes, on_delete=models.CASCADE, related_name='itens', verbose_name='Consultora')
-    data_criacao = models.DateField(auto_now_add=True)
+    data_criacao = models.DateField(auto_now_add=True,verbose_name= 'Criado em')
+    atualizacao = models.DateTimeField(auto_now=True, verbose_name='Atualizado em')
 
+    def __str__(self):
+        return self.nome
+    
     def valor_total(self):
         return self.produtos.aggregate(total=models.Sum('valor'))['total'] or 0
+    
+    def valor_total_formatado(self):
+        total = self.valor_total()
+        if total is None:
+            total = 0
+        return mark_safe(f"R$ {total:,.2f}".replace(",", "X").replace(".", ".").replace("X", "."))
+
+    valor_total_formatado.short_description = "VALOR TOTAL"
+    
+    def comissaoVendedora(self):
+        total = self.valor_total()
+        
+        if total >=1000:
+            porcentagem = Decimal('0.05')
+        elif total >=2000:
+            porcentagem = Decimal('0.10')
+        elif total >=3000:
+            porcentagem= Decimal('0.20')
+        else:
+            porcentagem = Decimal('0.00')#sem comissão
+        return round(total * porcentagem, 2)
     
     class Meta:
         verbose_name ='Mostruario'
@@ -45,6 +72,7 @@ class Produto(models.Model):
     maleta = models.ForeignKey(
         Maleta, on_delete=models.CASCADE, related_name='produtos')
     valor = models.DecimalField(max_digits=8, decimal_places=2)
+    # data_criacao
 
     def __str__(self):
         return f"{self.tipos} - R$ {self.valor}"
